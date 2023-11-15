791ec042e27f1059066ed8400578082470c1ab17
Raclax <raclax@Ordi.tout.court>
2023-11-15 11:24:36 +0100

    entrainelement

--- exercices/exercice_3.sh
+++ exercices/exercice_3.sh
@@ -1,5 +1,5 @@
 grep -o "\w*" candide.txt|tr [:upper:] [:lower:] | tr -d [:punct:] >> liste1.txt
 echo -e  "\r" >> liste2.txt
 grep -o "\w*" candide.txt|tr [:upper:] [:lower:] | tr -d [:punct:] >> liste2.txt
-paste liste2.txt liste1.txt | tail -n+2 | sort | uniq -c | sort -nr | head -n15
+paste liste2.txt liste1.txt | sort | uniq -c | sort -nr | head -n15
 
